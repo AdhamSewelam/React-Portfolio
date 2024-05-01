2f5691f215a4777b833a7b65e6fd02def7fd44ea
AdhamSewelam <adham.h.sewelam@gmail.com>
2024-05-02 01:56:26 +0300

    CLOSE SIDEBAR AFTER CHOOSE

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,17 @@
 // STYLES
-import { Container, Navbar, Offcanvas } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom";
-import styles from "./Sidebar.module.css";
-import { ABOUT, EXPERIENCE, ROOT, SKILLS, WORK } from "../Common/Constants";
+import { Container, Navbar, Offcanvas } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import styles from './Sidebar.module.css';
+import { ABOUT, EXPERIENCE, ROOT, SKILLS, WORK } from '../Common/Constants';
+import { useState } from 'react';
 
 function Sidebar() {
   const location = useLocation();
+  const [showOffcanvas, setShowOffcanvas] = useState(false); // State to control offcanvas visibility
+
+  const handleLinkClick = () => {
+    setShowOffcanvas(false); // Close offcanvas when a link is clicked
+  };
 
   return (
     <>
@@ -16,13 +22,21 @@ function Sidebar() {
         style={{ zIndex: 1000 }}
       >
         <Container fluid>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-expand-false`}
+            onClick={() => setShowOffcanvas(!showOffcanvas)}
+          />
           <Navbar.Offcanvas
+            show={showOffcanvas} // Control visibility based on state
+            onHide={() => setShowOffcanvas(false)}
             id={`offcanvasNavbar-expand-false`}
             aria-labelledby={`offcanvasNavbarLabel-expand-false`}
             placement="start"
           >
-            <Offcanvas.Header closeButton>
+            <Offcanvas.Header
+              closeButton
+              onHide={() => setShowOffcanvas(false)}
+            >
               <Offcanvas.Title
                 id={`offcanvasNavbarLabel-expand-false`}
               ></Offcanvas.Title>
@@ -44,46 +58,66 @@ function Sidebar() {
               <ul className={`${styles.sidebarList}`}>
                 <li
                   className={
-                    location.pathname === ROOT ? `${styles.active}` : ""
+                    location.pathname === ROOT ? `${styles.active}` : ''
                   }
                 >
-                  <Link to={ROOT} data-nav-section="home">
+                  <Link
+                    to={ROOT}
+                    onClick={handleLinkClick}
+                    data-nav-section="home"
+                  >
                     Home
                   </Link>
                 </li>
                 <li
                   className={
-                    location.pathname === ABOUT ? `${styles.active}` : ""
+                    location.pathname === ABOUT ? `${styles.active}` : ''
                   }
                 >
-                  <Link to={ABOUT} data-nav-section="about">
+                  <Link
+                    to={ABOUT}
+                    onClick={handleLinkClick}
+                    data-nav-section="about"
+                  >
                     About
                   </Link>
                 </li>
                 <li
                   className={
-                    location.pathname === SKILLS ? `${styles.active}` : ""
+                    location.pathname === SKILLS ? `${styles.active}` : ''
                   }
                 >
-                  <Link to={SKILLS} data-nav-section="skills">
+                  <Link
+                    to={SKILLS}
+                    onClick={handleLinkClick}
+                    data-nav-section="skills"
+                  >
                     Skills
                   </Link>
                 </li>
                 <li
                   className={
-                    location.pathname === EXPERIENCE ? `${styles.active}` : ""
+                    location.pathname === EXPERIENCE ? `${styles.active}` : ''
                   }
                 >
-                  <Link to={EXPERIENCE} data-nav-section="experience">
+                  <Link
+                    to={EXPERIENCE}
+                    onClick={handleLinkClick}
+                    data-nav-section="experience"
+                  >
                     Experience
                   </Link>
                 </li>
                 <li
                   className={
-                    location.pathname === WORK ? `${styles.active}` : ""
+                    location.pathname === WORK ? `${styles.active}` : ''
                   }
                 >
-                  <Link to={WORK} data-nav-section="work">
+                  <Link
+                    to={WORK}
+                    onClick={handleLinkClick}
+                    data-nav-section="work"
+                  >
                     Work
                   </Link>
                 </li>
@@ -105,18 +139,18 @@ function Sidebar() {
         </p>
 
         <ul className={`${styles.sidebarList}`}>
-          <li className={location.pathname === ROOT ? `${styles.active}` : ""}>
+          <li className={location.pathname === ROOT ? `${styles.active}` : ''}>
             <Link to={ROOT} data-nav-section="home">
               Home
             </Link>
           </li>
-          <li className={location.pathname === ABOUT ? `${styles.active}` : ""}>
+          <li className={location.pathname === ABOUT ? `${styles.active}` : ''}>
             <Link to={ABOUT} data-nav-section="about">
               About
             </Link>
           </li>
           <li
-            className={location.pathname === SKILLS ? `${styles.active}` : ""}
+            className={location.pathname === SKILLS ? `${styles.active}` : ''}
           >
             <Link to={SKILLS} data-nav-section="skills">
               Skills
@@ -124,14 +158,14 @@ function Sidebar() {
           </li>
           <li
             className={
-              location.pathname === EXPERIENCE ? `${styles.active}` : ""
+              location.pathname === EXPERIENCE ? `${styles.active}` : ''
             }
           >
             <Link to={EXPERIENCE} data-nav-section="experience">
               Experience
             </Link>
           </li>
-          <li className={location.pathname === WORK ? `${styles.active}` : ""}>
+          <li className={location.pathname === WORK ? `${styles.active}` : ''}>
             <Link to={WORK} data-nav-section="work">
               Work
             </Link>
